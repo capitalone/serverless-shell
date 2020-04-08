c1d13f8779597a89ccb28faca69e3c4fbc7ede5c
Calvin Mlynarczyk <calvin.mlynarczyk@capitalone.com>
2020-04-08 10:32:45 -0400

    Bump eslint to v5 (#4)
    * Create yarn lockfile
    * Fix linting errors

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-/* jshint ignore:start */
 'use strict';
 
 const {spawnSync} = require('child_process');
@@ -9,7 +8,6 @@ const _ = require('lodash');
  * Plugin for Serverless 1.x that drops you to a shell with your env vars!
  */
 class ServerlessLocalShell {
-
   /**
    * The plugin constructor
    * @param {Object} serverless
@@ -21,39 +19,42 @@ class ServerlessLocalShell {
     this.serverless = serverless;
     this.options = options;
     this.provider = this.serverless.getProvider(
-      this.serverless.service.provider.name);
+        this.serverless.service.provider.name);
 
     this.commands = {
       'shell': {
-         usage: 'Drop to a REPL with your environment variables properly set',
-         lifecycleEvents: [
-           'shell',
-         ],
-         options: {
-           function: {
-             usage: 'Specify the function whose environment you want (e.g. "--function myFunction")',
-             shortcut: 'f',
-             required: false,
-           },
-           shell: {
-             usage: 'Specify a different shell (e.g. "--shell bash")',
-             shortcut: 'S',
-             required: false,
-           },
-           quiet: {
-             usage: "Don't output anay log messages besides the subcommand's output",
-             shortcut: 'q',
-             required: false,
-             default: false,
-           },
-         },
+        usage: 'Drop to a REPL with your environment variables properly set',
+        lifecycleEvents: [
+          'shell',
+        ],
+        options: {
+          function: {
+            usage:
+              'Specify the function whose environment you want (e.g. ' +
+              '"--function myFunction")',
+            shortcut: 'f',
+            required: false,
+          },
+          shell: {
+            usage: 'Specify a different shell (e.g. "--shell bash")',
+            shortcut: 'S',
+            required: false,
+          },
+          quiet: {
+            usage:
+              `Don't output anay log messages besides the subcommand's output`,
+            shortcut: 'q',
+            required: false,
+            default: false,
+          },
+        },
       },
     };
 
     this.hooks = {
       'shell:shell': () => Promise.bind(this)
-        .then(this.loadEnvVars)
-        .then(this.shell),
+          .then(this.loadEnvVars)
+          .then(this.shell),
     };
   }
 
@@ -64,50 +65,64 @@ class ServerlessLocalShell {
     try {
       // from invoke local's extendedValidate
       this.options.functionObj = this.serverless.service.getFunction(
-        this.options.function);
+          this.options.function);
     } catch (e) {
       // skip if no function defined
       this.options.functionObj = {};
     }
 
     const lambdaName = this.options.functionObj.name;
-    const memorySize = Number(this.options.functionObj.memorySize)
-    || Number(this.serverless.service.provider.memorySize)
-    || 1024;
+    const memorySize = Number(this.options.functionObj.memorySize) ||
+    Number(this.serverless.service.provider.memorySize) ||
+    1024;
 
     const lambdaDefaultEnvVars = {
-    //PATH: '/usr/local/lib64/node-v4.3.x/bin:/usr/local/bin:/usr/bin/:/bin',
-    LANG: 'en_US.UTF-8',
-    LD_LIBRARY_PATH: '/usr/local/lib64/node-v4.3.x/lib:/lib64:/usr/lib64:/var/runtime:/var/runtime/lib:/var/task:/var/task/lib', // eslint-disable-line max-len
-    LAMBDA_TASK_ROOT: '/var/task',
-    LAMBDA_RUNTIME_DIR: '/var/runtime',
-    AWS_REGION: this.options.region || _.get(this.serverless, 'service.provider.region'),
-    AWS_DEFAULT_REGION: this.options.region || _.get(this.serverless, 'service.provider.region'),
-    AWS_LAMBDA_LOG_GROUP_NAME: this.provider.naming.getLogGroupName(lambdaName),
-    AWS_LAMBDA_LOG_STREAM_NAME:
+    // PATH: '/usr/local/lib64/node-v4.3.x/bin:/usr/local/bin:/usr/bin/:/bin',
+      LANG: 'en_US.UTF-8',
+      LD_LIBRARY_PATH: '/usr/local/lib64/node-v4.3.x/lib:/lib64:/usr/lib64:/var/runtime:/var/runtime/lib:/var/task:/var/task/lib', // eslint-disable-line max-len
+      LAMBDA_TASK_ROOT: '/var/task',
+      LAMBDA_RUNTIME_DIR: '/var/runtime',
+      AWS_REGION:
+        this.options.region ||
+        _.get(this.serverless, 'service.provider.region'),
+      AWS_DEFAULT_REGION:
+        this.options.region ||
+        _.get(this.serverless, 'service.provider.region'),
+      AWS_LAMBDA_LOG_GROUP_NAME:
+        this.provider.naming.getLogGroupName(lambdaName),
+      AWS_LAMBDA_LOG_STREAM_NAME:
       '2016/12/02/[$LATEST]f77ff5e4026c45bda9a9ebcec6bc9cad',
-    AWS_LAMBDA_FUNCTION_NAME: lambdaName,
-    AWS_LAMBDA_FUNCTION_MEMORY_SIZE: memorySize,
-    AWS_LAMBDA_FUNCTION_VERSION: '$LATEST',
-    NODE_PATH: '/var/runtime:/var/task:/var/runtime/node_modules',
+      AWS_LAMBDA_FUNCTION_NAME: lambdaName,
+      AWS_LAMBDA_FUNCTION_MEMORY_SIZE: memorySize,
+      AWS_LAMBDA_FUNCTION_VERSION: '$LATEST',
+      NODE_PATH: '/var/runtime:/var/task:/var/runtime/node_modules',
     };
 
     const providerEnvVars = this.serverless.service.provider.environment || {};
     const functionEnvVars = this.options.functionObj.environment || {};
 
-    let enterpriseCredsEnvVars = {};
+    const enterpriseCredsEnvVars = {};
     if (this.serverless.service.provider.name === 'aws') {
-      const { cachedCredentials } = this.serverless.getProvider('aws');
+      const {cachedCredentials} = this.serverless.getProvider('aws');
       if (cachedCredentials) {
-        enterpriseCredsEnvVars.AWS_ACCESS_KEY_ID = cachedCredentials.accessKeyId;
-        enterpriseCredsEnvVars.AWS_SECRET_ACCESS_KEY = cachedCredentials.secretAccessKey;
-        enterpriseCredsEnvVars.AWS_SESSION_TOKEN = cachedCredentials.sessionToken;
-        enterpriseCredsEnvVars.AWS_REGION = cachedCredentials.region;
+        enterpriseCredsEnvVars.AWS_ACCESS_KEY_ID =
+          cachedCredentials.accessKeyId;
+        enterpriseCredsEnvVars.AWS_SECRET_ACCESS_KEY =
+          cachedCredentials.secretAccessKey;
+        enterpriseCredsEnvVars.AWS_SESSION_TOKEN =
+          cachedCredentials.sessionToken;
+        enterpriseCredsEnvVars.AWS_REGION =
+          cachedCredentials.region;
       }
     }
 
     Object.assign(
-      process.env, lambdaDefaultEnvVars, providerEnvVars, functionEnvVars, enterpriseCredsEnvVars);
+        process.env,
+        lambdaDefaultEnvVars,
+        providerEnvVars,
+        functionEnvVars,
+        enterpriseCredsEnvVars
+    );
   }
   /**
    * load the right environment variables and start shell
@@ -115,7 +130,7 @@ class ServerlessLocalShell {
   shell() {
     let shellBinary;
     if (this.options.shell) {
-     shellBinary = this.options.shell;
+      shellBinary = this.options.shell;
     } else if (_.has(this.serverless, 'service.custom.shellBinary')) {
       shellBinary = this.serverless.service.custom.shellBinary;
     } else if (this.serverless.service.provider.runtime.startsWith('nodejs')) {
@@ -123,17 +138,18 @@ class ServerlessLocalShell {
     } else {
       shellBinary = this.serverless.service.provider.runtime;
     }
-      if (!this.options.quiet) {
-        this.serverless.cli.log(`Spawning ${shellBinary}...`);
-      }
-      if (process.env.SLS_DEBUG) {
-        console.log(`Setting env: ${JSON.stringify(process.env, null, 2)}`);
-      }
-      spawnSync(shellBinary, [], {
-        env: process.env,
-        stdio: 'inherit',
-        shell: true,
-      });
+    if (!this.options.quiet) {
+      this.serverless.cli.log(`Spawning ${shellBinary}...`);
+    }
+    if (process.env.SLS_DEBUG) {
+      // eslint-disable-next-line no-console
+      console.log(`Setting env: ${JSON.stringify(process.env, null, 2)}`);
+    }
+    spawnSync(shellBinary, [], {
+      env: process.env,
+      stdio: 'inherit',
+      shell: true,
+    });
   };
 
   /**
